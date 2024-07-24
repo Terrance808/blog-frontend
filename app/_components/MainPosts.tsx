e38317fec7e3c551cd9be8c539bd98c78bab48f6
Terrance <terrance@shiptection.com>
2024-07-24 16:04:02 -0400

    add blog front page

---
+++ app/_components/MainPosts.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+// import Markdown from './Markdown';
+
+
+interface Post {
+    title: string;
+    date: string;
+    content: string;
+  }
+
+interface MainProps {
+  posts: Post[];
+  title: string;
+}
+
+export default function Main(props: MainProps) {
+  const { posts, title } = props;
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      {posts.map((post, index) => (
+        // <Markdown className="markdown" key={post.substring(0, 40)}>
+        //   {post}
+        // </Markdown>
+        <div key={index} className="markdown">
+          <Typography variant="h6" gutterBottom>
+            {post.title}
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            {post.date}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {post.content}
+          </Typography>
+          <Divider />
+        </div>
+      ))}
+    </Grid>
+  );
+}
